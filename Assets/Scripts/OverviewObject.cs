4edea305ca571ff4514315d570ecc2aa2b4bde31
leegillan <charliegillan@live.com>
2021-08-25 06:27:51 +0100

    Added functionality of top grid.
    Camera moves between zones by choosing the zone from the overview grid.
    
    Pressing 4 takes you back to main grid.
    
    Created some more placeholder prefabs.
    
    Created an overview object script for selecting them checks.
    
    Changed material for overview grid slots.
    
    Adjusted alternate camera script to use the overview grid first and to move the camera when the zone is changed.
    Also to change the orthographic size due to differently sized grids so need  a less zoomed in view for the overview grid.
    
    Game now starts at the overview grid to select which zone to go to.
    
    Production and QA grids work just now.

--- Assets/Scripts/OverviewObject.cs
+++ Assets/Scripts/OverviewObject.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickableObject : MonoBehaviour
+public class OverviewObject : MonoBehaviour
 {
+    public ZoneDecider.ZONES zone;
 }
