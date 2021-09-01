7926cb59942f2f5d07b23aa2e705f9f238774b92
Austallan <1700403@uad.ac.uk>
2021-09-01 18:59:39 +0100

    Second Hand Shopfront
    Second hand machines are generated and displayed properly. Still need to add buy/build functionality

--- Assets/Scripts/SecondhandMarketScript.cs
+++ Assets/Scripts/SecondhandMarketScript.cs
@@ -8,20 +8,35 @@ public class SecondhandMarketScript : MonoBehaviour
 {
     bool secondhandSaleActive = false;
     //Type variable and getter
-    public ObjectInfo objecty;
+    public GameObject machinePage, secondhandPage, objectManager, secondHandPressy, secondHandQADesky, secondHandMelty;
 
-    public GameObject machinePage, secondhandPage;
+    public TextMeshProUGUI secondhandToggleButton, tab1Cost, tab1Condition, tab2Cost, tab2Condition, tab3Cost, tab3Condition;//tab1 - Pressers, tab2 - QA desks, tab3 - Melters
 
-    public TextMeshProUGUI tab1Cost, tab1Condition, tab2Cost, tab2Condition, tab3Cost, tab3Condition;
+    Data cost;
 
-    ObjectInfo[] secondHandPressers;
-    ObjectInfo[] secondHandQADesks;
-    ObjectInfo[] secondHandMelters;
+    
+
+    GameObject[] secondHandPressers = new GameObject[3];
+    GameObject[] secondHandQADesks = new GameObject[3];
+    GameObject[] secondHandMelters = new GameObject[3];
+
+    int pressNum, qaNum, meltNum;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pressNum = 0;
+        qaNum = 0;
+        meltNum = 0;
+
+        for (int i = 0; i < 3; i++)
+		{
+            secondHandPressers[i] = Instantiate(secondHandPressy);
+            secondHandQADesks[i] = Instantiate(secondHandQADesky);
+            secondHandMelters[i] = Instantiate(secondHandMelty);
+        }
+
+        RefreshSecondhandShop();
     }
 
     public void SecondhandToggle() //Toggles between showing the new machines and the second hand machines
@@ -31,31 +46,86 @@ public class SecondhandMarketScript : MonoBehaviour
             secondhandSaleActive = false;
             secondhandPage.SetActive(false);
             machinePage.SetActive(true);
+            secondhandToggleButton.SetText("Second hand");
         }
 		else
 		{
             secondhandSaleActive = true;
             secondhandPage.SetActive(true);
             machinePage.SetActive(false);
+            secondhandToggleButton.SetText("New");
         }
     }
 
     public void SetToFalse() { secondhandSaleActive = false; } //ensures that new machines are prioritised
 
+    public void cycleTab1()
+	{
+        if (pressNum < 2) { pressNum++; }
+        else { pressNum = 0; }
+        UpdateOutputs();
+    }
+    public void cycleTab2()
+    {
+        if (qaNum < 2) { qaNum++; }
+        else { qaNum = 0; }
+        UpdateOutputs();
+    }
+    public void cycleTab3()
+    {
+        if (meltNum < 2) { meltNum++; }
+        else { meltNum = 0; }
+        UpdateOutputs();
+    }
+
     public void RefreshSecondhandShop()
 	{
-        for (int i = 0; i < 3; i++)
-		{
-            secondHandMelters[i].SetIsMachine(true);
-            secondHandMelters[i].SetMachineHealth(Random.Range(50, 90));
-            secondHandMelters[i].constructionTime = 2.5f;
+        for (int i = 0; i < secondHandPressers.Length; i++)
+        {
+            //Pressers
+            secondHandPressers[i].GetComponent<ObjectInfo>().SetMachineHealth(Random.Range(50, 90));
+
+            Debug.Log(secondHandPressers[i].GetComponent<ObjectInfo>().GetMachineHealth());
         }
 
-        UpdateOutputs();//Call last for guaranteed newest info
+        Debug.Log(secondHandPressers[0].GetComponent<ObjectInfo>().GetMachineHealth());
+        Debug.Log(secondHandPressers[1].GetComponent<ObjectInfo>().GetMachineHealth());
+        Debug.Log(secondHandPressers[2].GetComponent<ObjectInfo>().GetMachineHealth());
+
+        for (int i = 0; i < secondHandQADesks.Length; i++)
+        {
+            //QA Desks
+            secondHandQADesks[i].GetComponent<ObjectInfo>().SetMachineHealth(90);
+        }
+
+        for (int i = 0; i < secondHandMelters.Length; i++)
+        {
+            //Melters
+            secondHandMelters[i].GetComponent<ObjectInfo>().SetMachineHealth(Random.Range(50, 90));
+        }
+    
+       UpdateOutputs();//Call last for guaranteed newest info
 	}
 
     void UpdateOutputs()
 	{
 
+        cost = objectManager.GetComponent<ObjectData>().GetObjectData(ObjectInfo.TYPE.PRESSER);//Get the base cost
+
+        //Pressers
+        tab1Cost.SetText("Cost: £" + (cost.purchaseCost * secondHandPressers[pressNum].GetComponent<ObjectInfo>().GetMachineHealth()) / 100);//Set appropriate discounted cost
+        tab1Condition.SetText("Status: " + secondHandPressers[pressNum].GetComponent<ObjectInfo>().GetMachineHealth().ToString() + "%");//Output the health percentage
+
+        cost = objectManager.GetComponent<ObjectData>().GetObjectData(ObjectInfo.TYPE.QADESK);
+
+        //QA Desks
+        tab2Cost.SetText("Cost: £" + (cost.purchaseCost * secondHandQADesks[qaNum].GetComponent<ObjectInfo>().GetMachineHealth()) / 100);
+        tab2Condition.SetText("Status: " + secondHandQADesks[qaNum].GetComponent<ObjectInfo>().GetMachineHealth().ToString() + "%");
+
+        cost = objectManager.GetComponent<ObjectData>().GetObjectData(ObjectInfo.TYPE.MELTER);
+
+        //Melters
+        tab3Cost.SetText("Cost: £" + (cost.purchaseCost * secondHandMelters[meltNum].GetComponent<ObjectInfo>().GetMachineHealth()) / 100);
+        tab3Condition.SetText("Status: " + secondHandMelters[meltNum].GetComponent<ObjectInfo>().GetMachineHealth().ToString() + "%");
 	}
 }
